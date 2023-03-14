58a4694408dde94770e29f0109c37a2256308b95
Ravindra Patel <ravindra.patel@bluepineapple.io>
2023-03-14 14:13:27 +0530

    Added mx-auto to have the div component in the center

--- components/ShowError.jsx
+++ components/ShowError.jsx
@@ -1,6 +1,6 @@
 const ShowError = () => {
     return (
-      <div class="flex items-center h-[15vh] m-4 justify-center max-w-2xl bg-brightRedLight border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <div class="flex items-center mx-auto h-[15vh] m-4 justify-center max-w-2xl bg-brightRedLight border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <p class="font-normal text-white dark:text-gray-400">
           <b>Network Error Occurred!</b>
         </p>
