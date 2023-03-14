27b3309056e5e7367f56ea305cc6bc0636c13e64
Ravindra Patel <ravindra.patel@bluepineapple.io>
2023-03-14 14:01:22 +0530

    Changed the alignment of the COmponent

--- components/ShowError.jsx
+++ components/ShowError.jsx
@@ -1,8 +1,8 @@
 const ShowError = () => {
     return (
-      <div class="flex my-2 max-w-2xl p-3 bg-brightRedLight border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <p class="font-normal w-full m-5 text-white dark:text-gray-400">
-          <b>Internal Error Occurred!</b>
+      <div class="flex items-center h-[15vh] m-4 justify-center max-w-2xl bg-brightRedLight border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <p class="font-normal text-white dark:text-gray-400">
+          <b>Network Error Occurred!</b>
         </p>
       </div>
     );
